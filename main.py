196471c278c48758f0310e2be0499b04d50dd31c
gordon-chu1905 <61052161+gordon-chu1905@users.noreply.github.com>
2022-11-16 14:23:14 +0800

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from hashlib import sha256
 Reference List:
 [1] https://www.youtube.com/watch?v=zVqczFZr124
 [2] https://www.activestate.com/blog/how-to-build-a-blockchain-in-python/
-[3] 
+[3] https://ithelp.ithome.com.tw/users/20119982/ironman/2255?page=1 <- more comprehensive tutorial, recommend refer this pls.
 """
 
 # Blockchain prototype - The basic content
@@ -27,9 +27,16 @@ class Block:
         hash_data = json.dumps(self.__dict__)
         return sha256(hash_data.encode()).hexdigest()
 
+
 class Blockchain:
     def __init__(self):
         self.chain = [self.generate_genesis_block()]
+        self.adjust_difficulty_blocks = 20
+        self.difficulty = 1
+        self.block_time = 30
+        self.mining_rewards = 10
+        self.block_limitation = 32
+        self.pending_transactions = []
         
     def generate_genesis_block(self):
         return(Block(0, time.time(), ["COMP4142 Group Project"], 1, 5, "0", "0"))
@@ -61,13 +68,24 @@ class Blockchain:
     def is_valid_proof(self, block: Block, block_hash: str):
         return (block_hash.startswith('0' * block.difficulty) and block_hash == block.hash_sha256())
     
-    def adjust_current_block_difficulty(self, block: Block, num_of_previous_block: int):
-        latest_block_timestamps = []
-        for loop_index in range(num_of_previous_block):
-            latest_block_timestamps.append(self.latest_nth_block(loop_index).timestamp)
-        current_block_timestamp = block.timestamp
-        return
+    def adjust_current_block_difficulty(self):
+        if len(self.chain) % self.adjust_difficulty_blocks != 1:
+            return self.difficulty
+        elif len(self.chain) <= self.adjust_difficulty_blocks:
+            return self.difficulty
+        else:
+            start = self.chain[-1*self.adjust_difficulty_blocks-1].timestamp
+            finish = self.chain[-1].timestamp
+            average_time_consumed = round((finish - start) / (self.adjust_difficulty_blocks), 2)
+            
+            if average_time_consumed > self.block_time:
+                print(f"Average block time:{average_time_consumed}s. Lower the difficulty")
+                self.difficulty -= 1
+            else:
+                print(f"Average block time:{average_time_consumed}s. High up the difficulty")
+                self.difficulty += 1
+            
+            return self.difficulty
 
 if __name__ == "__main__":
     Blockchain() 
-    
