1c8d46a160161b568d000098dd4b453be9b0094f
Martin <huim13a@gmail.com>
2022-11-23 20:43:27 +0800

    small fix

--- LatestState.py
+++ LatestState.py
@@ -1,8 +1,8 @@
 class LatestState:
-    def __init__(self, height):
+    def __init__(self, height, fullNodeList, neighborList):
         self.height = height
-        self.fullNodeList = []
-        self.neighborList = []
+        self.fullNodeList = fullNodeList
+        self.neighborList = neighborList
 
     def __str__(self):
         return str(self.__class__) + ": " + str(self.__dict__)
