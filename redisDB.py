966f25b022818f1f4c88aff8b998d78a9fed7906
Martin <huim13a@gmail.com>
2022-11-23 21:23:15 +0800

    update

--- redisDB.py
+++ redisDB.py
@@ -1,16 +1,29 @@
 import redis
 from redis import StrictRedis, ConnectionPool
+import pickle
+from LatestState import LatestState
 
 """
 For memory storage, a localhost redis database will be used as suggested.
 >pip install redis
+
+redis for windows:
+https://github.com/microsoftarchive/redis/releases?page=1
+
+reference:
+https://stackoverflow.com/questions/15219858/how-to-store-a-complex-object-in-redis-using-redis-py
 """
 
 try:
-    pool = ConnectionPool(host='localhost', port=6379, db=0, password='p@ssw0rd')
+    pool = ConnectionPool(host='localhost', port=6379, db=0)
     redis = StrictRedis(connection_pool=pool)
 except Exception:
     print("Connection Error: " + Exception)
 
-def redisPush(latestState):
-    redis.rpush("latestState",latestState)
+def redisPush(latest_state):
+    pickled_object = pickle.dumps(latest_state)
+    redis.set(latest_state.height, pickled_object)
+
+def redisGet(key):
+    unpacked_object = pickle.loads(redis.get(key))
+    return(unpacked_object)
